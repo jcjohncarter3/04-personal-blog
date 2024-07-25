bcaf1b92b9b84ee731c96cfe33da64f557236fed
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-07-24 20:29:28 -0500

    Second Commit

--- assets/js/form.js
+++ assets/js/form.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   
     const modeToggle = document.getElementById('mode-toggle');
-    modeToggle.addEventListener('click', function() {
-      document.body.classList.toggle('dark-mode');
-    });
+    modeToggle.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+      });
   });
